d3b312012f558401dfb149a6e6e0be678f216a0a
Ben <benjheng@gmail.com>
2015-10-17 04:53:25 +0800

    Separate development and production server scripts

--- server/index.js
+++ server/index.js
@@ -1,2 +1,6 @@
 require('babel/register');
-require('./server');
+if (process.env.NODE_ENV === "production") {
+    require('./server');
+} else {
+    require('./dev-server');
+}
--- server/server.js
+++ server/server.js
@@ -1,18 +1,8 @@
 import Express from 'express';
 import path from 'path';
 
-import webpack from 'webpack';
-import webpackDevMiddleware from 'webpack-dev-middleware';
-import webpackHotMiddleware from 'webpack-hot-middleware';
-import webpackConfig from '../webpack.config';
-
 const app = new Express();
 const port = 3000;
-
-// Use this middleware to set up hot module reloading via webpack.
-const compiler = webpack(webpackConfig);
-app.use(webpackDevMiddleware(compiler, { noInfo: true, publicPath: webpackConfig.output.publicPath }));
-app.use(webpackHotMiddleware(compiler));
 
 app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, 'index.html'));
