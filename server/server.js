a4af1b539e5f85f1502cc2fbe249c9817094b449
Ben <benjheng@gmail.com>
2015-10-17 03:13:15 +0800

    Replace gulp + browserify with webpack

---
+++ server/server.js
@@ -0,0 +1,48 @@
+import Express from 'express';
+
+import webpack from 'webpack';
+import webpackDevMiddleware from 'webpack-dev-middleware';
+import webpackHotMiddleware from 'webpack-hot-middleware';
+import webpackConfig from '../webpack.config';
+
+const app = new Express();
+const port = 3000;
+
+// Use this middleware to set up hot module reloading via webpack.
+const compiler = webpack(webpackConfig);
+app.use(webpackDevMiddleware(compiler, { noInfo: true, publicPath: webpackConfig.output.publicPath }));
+app.use(webpackHotMiddleware(compiler));
+
+// This is fired every time the server side receives a request
+app.use(handleRender);
+
+function handleRender(req, res) {
+    const initialState = {};
+    res.send(renderFullPage("", initialState));
+}
+
+function renderFullPage(html, initialState) {
+    return `
+    <!doctype html>
+    <html>
+      <head>
+        <title>Subreddit Analytics</title>
+      </head>
+      <body>
+        <div id="app">${html}</div>
+        <script>
+          window.__INITIAL_STATE__ = ${JSON.stringify(initialState)};
+        </script>
+        <script src="/static/bundle.js"></script>
+      </body>
+    </html>
+    `;
+}
+
+app.listen(port, (error) => {
+    if (error) {
+        console.error(error);
+    } else {
+        console.info(`==> Listening on port ${port}. Open up http://localhost:${port}/ in your browser.`);
+    }
+});
