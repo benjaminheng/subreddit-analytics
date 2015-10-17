8cd87a4040133913bd1d654cd40e949286caa116
Ben <benjheng@gmail.com>
2015-10-18 02:57:53 +0800

    Add thunk middleware to store

--- app/common/store/configureStore.js
+++ app/common/store/configureStore.js
@@ -1,8 +1,13 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 import rootReducer from '../reducers';
 
+const createStoreWithMiddleware = applyMiddleware(
+    thunk
+)(createStore);
+
 export default function configureStore() {
-    const store = createStore(rootReducer);
+    const store = createStoreWithMiddleware(rootReducer);
 
     if (module.hot) {
         // Enable Webpack hot module replacement for reducers
