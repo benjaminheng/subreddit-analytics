2dce3906535168945155a0b6db4aeda549d107b9
Ben <benjheng@gmail.com>
2016-01-30 12:58:04 +0800

    Add basic layout for search bar

--- app/common/components/SearchBar.js
+++ app/common/components/SearchBar.js
@@ -1,14 +1,21 @@
 import React, { Component, PropTypes } from 'react';
 
 export default class SearchBar extends Component {
+
+    handleSearch(e) {
+        const { onSearch } = this.props;
+        e.preventDefault();
+        onSearch(this.refs.searchinput);
+    }
+
     render() {
         const { onSearch } = this.props;
 
         return (
-            <div className='search-wrapper'>
-                <input type='text' placeholder='Username' className='search-bar' ref='searchbar' />
-                <button className='search-button' onClick={e => onSearch(this.refs.searchbar)}>Search</button>
-            </div>
+            <form onSubmit={e => this.handleSearch(e)} className='search-bar'>
+                <input type='text' name='search' placeholder='Search users' className='search-input' ref='searchinput' />
+                <button className='search-button' onClick={e => this.handleSearch(e)}>Search</button>
+            </form>
         );
     }
 }
