6e04495a085a9f8301763843cc308372a03237b8
Ben <benjheng@gmail.com>
2016-03-13 00:14:02 +0800

    Add utility to get markdown content

---
+++ app/common/utils/markdown.js
@@ -0,0 +1,44 @@
+import marked from 'marked';
+
+let renderer = new marked.Renderer();
+
+// Block level renderer methods
+
+['blockquote', 'heading', 'list', 'listitem', 'paragraph',
+'tablerow', 'tablecell', 'strong', 'em', 'codespan', 'del'].map(name => {
+    renderer[name] = textPassthrough;
+});
+
+['hr', 'code', 'image'].map(name => {
+    renderer[name] = ignoreMarkup;
+});
+
+renderer.table = (header, body) => {
+    return `${header} ${body}`;
+}
+
+renderer.link = (href, title, text) => {
+    return text;
+}
+
+function ignoreMarkup() {
+    return '';
+}
+
+function textPassthrough(text) {
+    return `${text} `;
+}
+
+// Returns a space-separated list of words
+function getWords(markdownString) {
+    let words = marked(markdownString, {
+        renderer: renderer,
+        sanitize: true
+    });
+    words = words.replace(/\s{2,}/g, ' ').trim();
+    return words;
+}
+
+export default {
+    getWords
+}
--- test/utils_spec.js
+++ test/utils_spec.js
@@ -1,5 +1,6 @@
 import { expect } from 'chai';
 import date from '../app/common/utils/date';
+import markdown from '../app/common/utils/markdown';
 
 describe('utils', () => {
     describe('date', () => {
@@ -31,6 +32,107 @@ describe('utils', () => {
             expect(dateObj.getDate()).to.equal(1);
             expect(dateObj.getHours()).to.equal(15);
             expect(dateObj.getMinutes()).to.equal(0);
+        });
+    });
+    describe('markdown', () => {
+        it('ignores code blocks', () => {
+            const text = `
+\`\`\`
+function log(text) {
+    console.log(text);
+}
+\`\`\`
+            `;
+            const words = markdown.getWords(text);
+            expect(words).to.equal('');
+        });
+
+        it('removes blockquote markup', () => {
+            const text = `> Quoted text`;
+            const words = markdown.getWords(text);
+            expect(words).to.equal('Quoted text');
+        });
+
+        it('sanitizes html markup', () => {
+            const text = `Click <a href='#'>here</a>`;
+            const words = markdown.getWords(text);
+            expect(words).to.equal('Click &lt;a href=&#39;#&#39;&gt;here&lt;/a&gt;');
+        });
+
+
+        it('removes heading markup', () => {
+            const text = `# Title of post`;
+            const words = markdown.getWords(text);
+            expect(words).to.equal('Title of post');
+        });
+
+        it('removes horizontal-rule markup', () => {
+            const text = `-----`;
+            const words = markdown.getWords(text);
+            expect(words).to.equal('');
+        });
+
+        it('removes table markup', () => {
+            const text = `
+Foo | Bar
+---|---
+Fizz | Buzz
+            `;
+            const words = markdown.getWords(text);
+            expect(words).to.equal('Foo Bar Fizz Buzz');
+        });
+
+        it('removes bold (strong) markup', () => {
+            const text = `**Bolded text**`;
+            const words = markdown.getWords(text);
+            expect(words).to.equal('Bolded text');
+        });
+
+        it('removes emphasis (em) markup', () => {
+            const text = `*Emphasized text*`;
+            const words = markdown.getWords(text);
+            expect(words).to.equal('Emphasized text');
+        });
+
+        it('removes codespan markup', () => {
+            const text = `API: \`GET /users\``;
+            const words = markdown.getWords(text);
+            expect(words).to.equal('API: GET /users');
+        });
+
+        it('removes strikethrough markup', () => {
+            const text = `~~Strikethrough text~~`;
+            const words = markdown.getWords(text);
+            expect(words).to.equal('Strikethrough text');
+        });
+
+        it('removes link markup', () => {
+            const text = `[My profile](http://example.com/profile)`;
+            const words = markdown.getWords(text);
+            expect(words).to.equal('My profile');
+        });
+
+        it('ignores image markup', () => {
+            const text = `![My picture](http://example.com/picture)`;
+            const words = markdown.getWords(text);
+            expect(words).to.equal('');
+        });
+
+        it('extracts text from markdown', () => {
+            const text = `
+# Title of post
+
+**Bolded text**
+
+- List item 1
+- List item 2
+
+\`\`\`python
+def do_nothing():
+    pass
+\`\`\`
+            `;
+            const words = markdown.getWords(text);
         });
     });
 });
